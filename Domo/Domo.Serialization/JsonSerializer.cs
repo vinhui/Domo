a4bc00b38bf7241addb483dab04f5084fa3bd1c5
vinhui <vinni.vinni.vin@gmail.com>
2016-12-22 11:53:01 +0100

    Created very basic template for serialization

---
+++ Domo/Domo.Serialization/JsonSerializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Domo.Serialization
+{
+    public class JsonSerializer : Serializer
+    {
+        public override string Serialize(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public override T Deserialize<T>(string obj)
+        {
+            return JsonConvert.DeserializeObject<T>(obj);
+        }
+
+        public override object Deserialize(Type type, string obj)
+        {
+            return JsonConvert.DeserializeObject(obj, type);
+        }
+    }
+}
---
+++ Domo/Domo.Serialization/Serializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Domo.Serialization
+{
+    public abstract class Serializer
+    {
+        private static Serializer _instance;
+        public static Serializer instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new JsonSerializer();
+
+                return _instance;
+            }
+        }
+
+        public abstract string Serialize(object obj);
+
+        public virtual T Deserialize<T>(string obj)
+        {
+            return (T)Deserialize(typeof(T), obj);
+        }
+
+        public abstract object Deserialize(Type type, string obj);
+    }
+}
