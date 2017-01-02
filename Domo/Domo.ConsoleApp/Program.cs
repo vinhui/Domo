9a532a594409efe727f715112caa3b4f039ecde4
vinhui <vinni.vinni.vin@gmail.com>
2017-01-02 16:01:08 +0100

    Added config functioning checking

--- Domo/Domo.ConsoleApp/Program.cs
+++ Domo/Domo.ConsoleApp/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Domo.ConsoleApp
+﻿using Domo.Main;
+using Domo.Main.Debug;
+using System;
+
+namespace Domo.ConsoleApp
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+#if DEBUG
+            Log.Debug("Confirming config works correctly");
+            Config.SetValue(true, false, "test", "test");
+
+            if (Config.GetValue<bool>("test", "test"))
+                Log.Debug("Config works properly");
+            else
+                Log.Error("Config doesn't work properly!");
+#endif
+            Console.ReadKey();
         }
     }
 }
