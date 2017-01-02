d6c54378696261cec8d0a4597b701ac33f906ace
vinhui <vinni.vinni.vin@gmail.com>
2017-01-02 15:21:12 +0100

    Created basic webapi class

---
+++ Domo/Domo.API/WebAPI.cs
@@ -0,0 +1,28 @@
+﻿using Nancy;
+using System;
+
+namespace Domo.API
+{
+    public class WebAPI : NancyModule, IApiBase
+    {
+        public WebAPI()
+        {
+        }
+
+        public void Init()
+        {
+        }
+
+        public void OnShutdown()
+        {
+        }
+
+        public void RegisterListener(string key, Action listener)
+        {
+        }
+
+        public void UnregisterListener(string key, Action listener)
+        {
+        }
+    }
+}
