8ed989e682e309019053f687b19b2f80885e4303
vinhui <vinni.vinni.vin@gmail.com>
2017-01-04 11:51:04 +0100

    Added test for serialization

---
+++ Domo/Domo.Testing/SerializationTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domo.Serialization;
+
+namespace Domo.Testing
+{
+    [TestClass]
+    public class SerializationTest
+    {
+        [TestMethod]
+        public void GeneralSerialization()
+        {
+            var test = new TestObject()
+            {
+                name = "test",
+                value = 313
+            };
+            
+            string json = Serializer.instance.Serialize(test);
+            Console.WriteLine(json);
+            Assert.AreEqual(test, Serializer.instance.Deserialize<TestObject>(json));
+        }
+
+        public class TestObject
+        {
+            public string name;
+            public int value;
+
+            public override bool Equals(object obj)
+            {
+                TestObject o = obj as TestObject;
+                return o != null && o.name == name && o.value == value;
+            }
+
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+        }
+    }
+}
