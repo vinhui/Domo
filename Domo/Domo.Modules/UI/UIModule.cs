0b0261b90e2af74f152477b783945fe078b1a5c5
vinhui <vinni.vinni.vin@gmail.com>
2017-01-09 12:08:44 +0100

    The ui module now temporarily keeps track of all the modules

--- Domo/Domo.Modules/UI/UIModule.cs
+++ Domo/Domo.Modules/UI/UIModule.cs
@@ -1,11 +1,34 @@
 ﻿using Domo.Modules.UI.Controls;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Domo.Modules.UI
 {
     public class UIModule
     {
+        // TODO: All the modules probably shouldn't be kept in here
+        private static List<UIModule> modules = new List<UIModule>();
+        private static List<IUIModule> moduleInterfaces = new List<IUIModule>();
+
+        public static void RegisterModule(UIModule module)
+        {
+            modules.Add(module);
+        }
+        public static void RegisterModule(IUIModule module)
+        {
+            moduleInterfaces.Add(module);
+        }
+        public static IEnumerable<UIModule> GetModules()
+        {
+            List<UIModule> m = new List<UIModule>(modules.Count + moduleInterfaces.Count);
+
+            m.AddRange(modules);
+            m.AddRange(moduleInterfaces.Select(x => x.GetUI()));
+
+            return m;
+        }
+
         public ModuleBase owner { get; private set; }
 
         public string title;
