71c195b2529670aea59c32819b65f7ae1accdbe4
vinhui <vinni.vinni.vin@gmail.com>
2017-01-09 12:06:18 +0100

    Created helper functions for the api response

--- Domo/Domo.API/ApiResponse.cs
+++ Domo/Domo.API/ApiResponse.cs
@@ -12,10 +12,12 @@ namespace Domo.API
         /// See <see cref="ApiCodes"/> for the meaning of the codes
         /// </summary>
         public int code;
+
         /// <summary>
         /// If the request was successfully processed
         /// </summary>
         public bool success;
+
         /// <summary>
         /// The data that gets returned by the corresponding API listener
         /// </summary>
@@ -28,6 +30,74 @@ namespace Domo.API
         public string Serialize()
         {
             return Serializer.instance.Serialize(this);
+        }
+
+        /// <summary>
+        /// Create a success response
+        /// </summary>
+        /// <returns>Returns the created api response</returns>
+        public static ApiResponse Success()
+        {
+            return Success(null);
+        }
+
+        /// <summary>
+        /// Create a success response
+        /// </summary>
+        /// <param name="data">Data to add</param>
+        /// <returns>Returns the created api response</returns>
+        public static ApiResponse Success(Dictionary<string, object> data)
+        {
+            return Success(ApiCodes.Sucess, data);
+        }
+
+        /// <summary>
+        /// Create a success response
+        /// </summary>
+        /// <param name="code">Code to return, see <seealso cref="ApiCodes"/></param>
+        /// <param name="data">Data to add</param>
+        /// <returns>Returns the created api response</returns>
+        public static ApiResponse Success(int code, Dictionary<string, object> data)
+        {
+            return new ApiResponse()
+            {
+                success = true,
+                code = code,
+                data = data
+            };
+        }
+
+        /// <summary>
+        /// Create a failed response
+        /// </summary>
+        /// <param name="code">Code to return, see <seealso cref="ApiCodes"/></param>
+        /// <param name="reason">The reason why it failed</param>
+        /// <returns>Returns the created api response</returns>
+        public static ApiResponse Failed(int code, string reason)
+        {
+            return Failed(code, reason, null);
+        }
+
+        /// <summary>
+        /// Create a failed response
+        /// </summary>
+        /// <param name="code">Code to return, see <seealso cref="ApiCodes"/></param>
+        /// <param name="reason">The reason why it failed</param>
+        /// <param name="data">Extra data to add</param>
+        /// <returns>Returns the created api response</returns>
+        public static ApiResponse Failed(int code, string reason, Dictionary<string, object> data)
+        {
+            if (data == null)
+                data = new Dictionary<string, object>();
+
+            data.Add("reason", reason);
+
+            return new ApiResponse()
+            {
+                success = false,
+                code = code,
+                data = data
+            };
         }
     }
 }
