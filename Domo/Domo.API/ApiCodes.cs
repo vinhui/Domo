d2f9a300b18489280f8491b19ff1b159d0716e39
vinhui <vinni.vinni.vin@gmail.com>
2017-01-09 12:07:45 +0100

    Added extra api codes

--- Domo/Domo.API/ApiCodes.cs
+++ Domo/Domo.API/ApiCodes.cs
@@ -5,7 +5,9 @@
     /// </summary>
     public static class ApiCodes
     {
+        public const int Sucess = 0;
         public const int NoKeyProvided = 100;
         public const int UnknownKey = 101;
+        public const int NotEnoughData = 200;
     }
 }
