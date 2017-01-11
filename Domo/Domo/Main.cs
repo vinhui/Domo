209f90825fa1bd45d333bd94eea90136b2f668d2
vinhui <vinni.vinni.vin@gmail.com>
2017-01-11 14:07:20 +0100

    Added a main project thats repsonsible for starting and shutting down everything

---
+++ Domo/Domo/Main.cs
@@ -0,0 +1,17 @@
+﻿using Domo.API;
+
+namespace Domo
+{
+    public static class Main
+    {
+        public static void Init()
+        {
+            ApiManager.Init();
+        }
+
+        public static void ShutDown()
+        {
+            ApiManager.OnShutdown();
+        }
+    }
+}
