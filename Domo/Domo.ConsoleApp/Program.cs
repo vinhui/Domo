ae8f145a825eb9408ac536e68cd79a95c99d3b23
vinhui <vinni.vinni.vin@gmail.com>
2017-01-11 13:51:31 +0100

    Made sure the api onshutdown gets called properly

--- Domo/Domo.ConsoleApp/Program.cs
+++ Domo/Domo.ConsoleApp/Program.cs
@@ -8,11 +8,17 @@ namespace Domo.ConsoleApp
         private static void Main(string[] args)
         {
             ApiManager.Init();
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
 
             Console.ReadKey();
             ApiManager.OnShutdown();
 
             Console.ReadKey();
+        }
+
+        private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            ApiManager.OnShutdown();
         }
     }
 }
