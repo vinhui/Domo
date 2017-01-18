4464dfbe381f2a77e21b1aa44c0639e975169a87
vinhui <vinni.vinni.vin@gmail.com>
2017-01-18 15:07:57 +0100

    Attribute that will make sure the generic type of a property is autofilled

---
+++ Domo/Domo.Modules/AutoFillGenericAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domo.Modules
+{
+    /// <summary>
+    /// Attribute to put on properties with the same type as the generics in the class
+    /// This will automatically populate the property with an instance of the generic type
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class AutoFillGenericAttribute : Attribute
+    {
+    }
+}
