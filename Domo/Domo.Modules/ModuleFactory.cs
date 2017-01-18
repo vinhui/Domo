795569a4da5b67005b760ce4efdc69e78592af4d
vinhui <vinni.vinni.vin@gmail.com>
2017-01-18 15:08:44 +0100

    Created a modulemanager and modulefactory

---
+++ Domo/Domo.Modules/ModuleFactory.cs
@@ -0,0 +1,129 @@
+﻿using Domo.Misc.Debug;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Domo.Modules
+{
+    /// <summary>
+    /// Class responsibe for loading and keeping references to all the modules
+    /// </summary>
+    public class ModuleFactory : IDisposable
+    {
+        private Dictionary<Type, ModuleBase> modules = new Dictionary<Type, ModuleBase>();
+
+        /// <summary>
+        /// Load all the modules from an assembly
+        /// </summary>
+        /// <param name="assembly">Assembly to load the modules from</param>
+        /// <exception cref="ArgumentException">Gets thrown when the type is already loaded</exception>
+        public void LoadModules(Assembly assembly)
+        {
+            foreach (var item in assembly.GetTypes())
+            {
+                try
+                {
+                    CheckType(item);
+                    ModuleBase module = (ModuleBase)Activator.CreateInstance(item);
+                    modules.Add(item, module);
+                }
+                catch (TypeLoadException) { }
+            }
+
+            foreach (var item in modules)
+            {
+                FixReferences(item.Value);
+            }
+
+            // All the references should be assigned before going into the OnEnable
+            foreach (var item in modules)
+            {
+                item.Value.OnEnable();
+            }
+        }
+
+        /// <summary>
+        /// Get an instance of a module
+        /// </summary>
+        /// <typeparam name="T">Type of module to get an instance from</typeparam>
+        /// <returns>Returns the instance</returns>
+        public T GetInstance<T>() where T : ModuleBase
+        {
+            return (T)GetInstance(typeof(T));
+        }
+
+        /// <summary>
+        /// Get an instance of a module
+        /// </summary>
+        /// <param name="t">Type of module to get an instance from</param>
+        /// <returns>Returns the instance</returns>
+        public ModuleBase GetInstance(Type t)
+        {
+            CheckType(t);
+
+            if (modules.ContainsKey(t))
+                return modules[t];
+            else
+                throw new KeyNotFoundException("The module you're trying to get an instance of has not been loaded");
+        }
+
+        /// <summary>
+        /// Goes through all the properties in the module and fixes all the properties with the <see cref="AutoFillGenericAttribute"/> attribute
+        /// </summary>
+        /// <param name="module">Module to fix the references for</param>
+        private void FixReferences(ModuleBase module)
+        {
+            Type type = module.GetType();
+            PropertyInfo[] properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy);
+
+            foreach (var item in properties)
+            {
+                if (item.IsDefined(typeof(AutoFillGenericAttribute), true))
+                {
+                    // Make sure there is something to assign the property with
+                    Type parentType = item.DeclaringType;
+                    if (parentType.ContainsGenericParameters)
+                    {
+                        Type propType = item.PropertyType;
+
+                        // Bool that gets set when the property is sucessfully set, this is for logging purposes
+                        bool set = false;
+                        foreach (var generic in parentType.GenericTypeArguments)
+                        {
+                            if (generic == propType)
+                            {
+                                item.SetValue(module, GetInstance(propType));
+                                set = true;
+                                break;
+                            }
+                        }
+                        if (!set)
+                            Log.Warning("Property '{0}' in '{1}' has the '{2}' attribute but is not of the same type as one of the class generics", item.Name, parentType.FullName, typeof(AutoFillGenericAttribute).Name);
+                    }
+                    else
+                        Log.Warning("Property '{0}' in '{1}' has the '{2}' attribute but is not in a generic class!", item.Name, parentType.FullName, typeof(AutoFillGenericAttribute).Name);
+                }
+            }
+        }
+
+        private void CheckType(Type t)
+        {
+            if (t.IsSubclassOf(typeof(ModuleBase)))
+                throw new TypeLoadException(string.Format("The type of module you're trying to get an instance ({0}) of does not derive of '{1}'", t.FullName, typeof(ModuleBase).FullName));
+            if (t.IsAbstract)
+                throw new TypeLoadException(string.Format("The module of type '{0}' that you're trying to get an instance of is abstract", t.FullName));
+        }
+
+        /// <summary>
+        /// Calls the OnDisable of all the modules and clears the list of modules
+        /// </summary>
+        public void Dispose()
+        {
+            foreach (var item in modules)
+            {
+                item.Value.OnDisable();
+            }
+            modules.Clear();
+        }
+    }
+}
---
+++ Domo/Domo.Modules/ModuleManager.cs
@@ -0,0 +1,63 @@
+﻿using Domo.Misc.Debug;
+using System;
+using System.Reflection;
+
+namespace Domo.Modules
+{
+    /// <summary>
+    /// Class responsibe for all the module related things
+    /// </summary>
+    public static class ModuleManager
+    {
+        private static ModuleFactory factory;
+
+        /// <summary>
+        /// Initialize the manager
+        /// </summary>
+        public static void Init()
+        {
+            if (factory != null)
+                Log.Warning("ModuleManager has already been initialized, this can result in strange behaviour!");
+
+            factory = new ModuleFactory();
+        }
+
+        /// <summary>
+        /// Load all the modules in the current appdomain
+        /// </summary>
+        public static void LoadAllModules()
+        {
+            foreach (var item in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                LoadModules(item);
+            }
+        }
+
+        /// <summary>
+        /// Load modules from a single assembly
+        /// </summary>
+        /// <param name="assembly">Assembly to load modules from</param>
+        public static void LoadModules(Assembly assembly)
+        {
+            factory.LoadModules(assembly);
+        }
+
+        /// <summary>
+        /// Get a reference to a module of type <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T">Type to get a reference of</typeparam>
+        /// <returns>Returns an instance</returns>
+        public static T GetModuleReference<T>() where T : ModuleBase
+        {
+            return factory.GetInstance<T>();
+        }
+
+        /// <summary>
+        /// Properly dispose of all the modules
+        /// </summary>
+        public static void ShutDown()
+        {
+            factory.Dispose();
+        }
+    }
+}
