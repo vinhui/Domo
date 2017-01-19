e12e76a86ec307e9605d697edc7eb89cfbe1b18d
vinhui <vinni.vinni.vin@gmail.com>
2017-01-19 10:14:43 +0100

    Added the python packages to the packaging project

---
+++ Domo/Domo.Packaging/Packages/Test - Copy/0.1/Test.py
@@ -0,0 +1,14 @@
+#import Temp;
+#import File from System.IO
+#from System import Environment
+import System.IO
+
+class Test2:
+    x = 10;
+    b = "Hello";
+
+    def Hello():
+        print("Hello World");
+
+test = Temp();
+print(System.IO.Directory.GetCurrentDirectory());
---
+++ Domo/Domo.Packaging/Packages/Test2/0.1/Temp.py
@@ -0,0 +1,6 @@
+class Temp:
+    x = 10;
+    b = "Hello";
+
+    def __init__(self):
+        print("Hello World");
