6b098fddf0b9b7b8659a0c5349514581e3fdee8f
vinhui <vinni.vinni.vin@gmail.com>
2017-01-31 11:31:52 +0100

    Created overloaded helper methods for the apirequest

--- Domo/Domo.API/ApiRequest.cs
+++ Domo/Domo.API/ApiRequest.cs
@@ -23,10 +23,62 @@ namespace Domo.API
         /// </summary>
         /// <typeparam name="T">Type to cast the argument to</typeparam>
         /// <param name="key">Key of the argument</param>
+        /// <param name="defaultValue">The default value</param>
+        /// <returns>Retuns the value or <paramref name="defaultValue"/> if the arguments don't contain the key</returns>
+        public T GetArgument<T>(string key, T defaultValue)
+        {
+            if (HasArgument(key))
+                return GetArgument<T>(key);
+            else
+                return defaultValue;
+        }
+
+        /// <summary>
+        /// Get an argument
+        /// </summary>
+        /// <typeparam name="T">Type to cast the argument to</typeparam>
+        /// <param name="key">Key of the argument</param>
         /// <returns>Retuns the value or default(<typeparamref name="T"/>)</returns>
         public T GetArgument<T>(string key)
         {
-            return (T)Convert.ChangeType(arguments[key], typeof(T));
+            if (HasArgument(key))
+                return (T)Convert.ChangeType(arguments[key], typeof(T));
+            else
+                return default(T);
+        }
+
+        /// <summary>
+        /// Get an argument
+        /// </summary>
+        /// <param name="key">Key of the argument</param>
+        /// <returns>Returns the value or null if the arguments don't contain the key</returns>
+        public object GetArgument(string key)
+        {
+            return GetArgument(key, null);
+        }
+
+        /// <summary>
+        /// Get an argument
+        /// </summary>
+        /// <param name="key">Key of the argument</param>
+        /// <param name="defaultValue">Default value</param>
+        /// <returns>Returns the value or <paramref name="defaultValue"/> if the arguments don't contain the key</returns>
+        public object GetArgument(string key, object defaultValue)
+        {
+            if (HasArgument(key))
+                return arguments[key];
+            else
+                return defaultValue;
+        }
+
+        /// <summary>
+        /// Check if the arguments contain a certain key
+        /// </summary>
+        /// <param name="key">Key to look for</param>
+        /// <returns></returns>
+        public bool HasArgument(string key)
+        {
+            return arguments.ContainsKey(key);
         }
     }
 }
