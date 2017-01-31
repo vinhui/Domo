b7b8086ec9300ce07a192e73b26e307bb1047e5c
vinhui <vinni.vinni.vin@gmail.com>
2017-01-31 11:31:27 +0100

    Made apirequest arguments readonly and added comments to the method

--- Domo/Domo.API/ApiRequest.cs
+++ Domo/Domo.API/ApiRequest.cs
@@ -16,8 +16,14 @@ namespace Domo.API
         /// <summary>
         /// Arguments from the request
         /// </summary>
-        public IDictionary<string, object> arguments = new Dictionary<string, object>();
+        public IReadOnlyDictionary<string, object> arguments = new Dictionary<string, object>();
 
+        /// <summary>
+        /// Get an argument
+        /// </summary>
+        /// <typeparam name="T">Type to cast the argument to</typeparam>
+        /// <param name="key">Key of the argument</param>
+        /// <returns>Retuns the value or default(<typeparamref name="T"/>)</returns>
         public T GetArgument<T>(string key)
         {
             return (T)Convert.ChangeType(arguments[key], typeof(T));
