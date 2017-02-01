9916ab3b0c087b0664b524b19cd4b15197829d67
vinhui <vinni.vinni.vin@gmail.com>
2017-02-01 15:25:33 +0100

    Created modules for checking lan devices

---
+++ Domo/Domo.Packaging/Packages/ModuleTest/0.1/LanSensor.py
@@ -0,0 +1,72 @@
+﻿from Domo.Modules import *
+
+from System.Threading import Thread, ThreadStart
+
+class LanSensor(SensorModule):
+	devices = []
+	onScanDone = []
+	
+	portScanner = None
+	scanHosts = "192.168.0.0/24"
+	#scanArgs = "-n -sP -T4"
+	scanArgs = "-n -sP"
+
+	scanningThread = None
+
+	scanInterval = 1000
+
+	def OnEnable(self):
+		self.portScanner = PortScanner()
+
+		self.scanningThread = None
+		self.scanningThread = Thread(ThreadStart(self.scanningLoop))
+		self.scanningThread.Start()
+		pass
+
+	def OnDisable(self):
+		if self.scanningThread is not None:
+			self.scanningThread.Abort()
+
+		self.portScanner = None
+		pass
+
+	def registerCallback(self, call):
+		self.onScanDone.append(call)
+		pass
+
+	def unregisterCallback(self, call):
+		self.onScanDone.remove(call)
+		pass
+
+	def scanningLoop(self):
+		if self.portScanner is not None:
+			self.devices = []
+			result = self.portScanner.scan(hosts=self.scanHosts, arguments=self.scanArgs)
+			Log.Debug("Nmap scan cycle done")
+			if result is not None:
+				for ip, obj in result["scan"].iteritems():
+					if "mac" in obj["addresses"]:
+						self.devices.append(LanDevice(obj))
+
+			for i in self.onScanDone:
+				i()
+
+			Thread.Sleep(self.scanInterval)
+			self.scanningLoop()
+		pass
+
+class LanDevice:
+	ip = ""
+	mac = ""
+	vendor = ""
+
+	def __init__(self):
+		pass
+
+	def __init__(self, nmapObj):
+		self.ip = nmapObj["addresses"]["ipv4"]
+		self.mac = nmapObj["addresses"]["mac"]
+
+		if len(nmapObj["vendor"]) > 0:
+			self.vendor = nmapObj["vendor"][self.mac]
+		pass
---
+++ Domo/Domo.Packaging/Packages/ModuleTest/0.1/LanTrigger.py
@@ -0,0 +1,76 @@
+﻿from Domo.Modules import *
+
+class LanTrigger(SensorTriggerModule):
+	triggerOnFirst = True
+	
+	knownDevices = []
+	triggerDevices = ["C0:EE:FB:ED:6B:98"]
+
+	joinedDevices = []
+	leftDevices = []
+
+	onDeviceJoined = []
+	onDeviceLeft = []
+
+	def __init__(self):
+		self.onDeviceJoined.append(self.deviceJoined)
+		self.onDeviceLeft.append(self.deviceLeft)
+
+		if not self.triggerOnFirst:
+			self.isFirstRun = True
+		pass
+
+	def OnEnable(self):
+		SensorTriggerModule.init(self, LanSensor)
+		self.sensor.registerCallback(self.onListUpdated)
+		pass
+
+	def OnDisable(self):
+		pass
+
+	def onListUpdated(self):
+		self.compareDevices(self.knownDevices, self.sensor.devices)
+		self.knownDevices = self.sensor.devices
+
+		if not self.triggerOnFirst:
+			if self.isFirstRun:
+				self.isFirstRun = False
+				return
+
+		self.eventCheck(self.joinedDevices, self.triggerDevices, self.onDeviceJoined)
+		self.eventCheck(self.leftDevices, self.triggerDevices, self.onDeviceLeft)
+		pass
+
+	def eventCheck(self, devices, triggerDevices, callbacks):
+		for device in devices:
+			if len(triggerDevices) > 0 and device.mac not in triggerDevices:
+				continue
+
+			self.OnTrigger()
+			for c in callbacks:
+				c(device)
+		pass
+
+	def compareDevices(self, oldList, newList):
+		self.joinedDevices = []
+		self.leftDevices = []
+
+		newListMacs = [x.mac for x in newList]
+		oldListMacs = [x.mac for x in oldList]
+
+		for i, d in enumerate(newListMacs):
+			if d not in oldListMacs:
+				self.joinedDevices.append(newList[i])
+
+		for i, d in enumerate(oldListMacs):
+			if d not in newListMacs:
+				self.leftDevices.append(oldList[i])
+		pass
+
+	def deviceJoined(self, device):
+		Log.Info("A lan device joined ({0})", device.mac)
+		pass
+
+	def deviceLeft(self, device):
+		Log.Info("A lan device left ({0})", device.mac)
+		pass
