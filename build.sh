66837ee10ccd02e72ea689f7b280c2729d4b548c
vinhui <vinni.vinni.vin@gmail.com>
2017-03-27 20:48:13 +0200

    Updated the build script

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 git submodule update --init --recursive
 cd "IronPython/"
 git pull origin ipy-2.7-maint
-xbuild "Solutions/IronPython.sln" /p:Configuration=Debug
+xbuild "Solutions/IronPython.sln" /p:Configuration=v45Debug
 
 cd ../
 
-git pull
+git pull origin Mono
+cp -ufR IronPython/bin/v45Debug/* Domo/Domo.ConsoleApp/bin/Debug/
+
 xbuild Domo/Domo.sln /p:Configuration=DebugNoTesting
